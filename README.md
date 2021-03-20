cf3f8cdb476777ee8e3e1e7f8b690daeda2b00e8
Denis Onofras <onofras.denisd@gmail.com>
2021-03-20 17:44:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SeeFood
+
+SeeFood app is developed using CoreML and Inceptionv3 model. The app use camera to take a picture and then it shows the name of the food.
---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Denis Onofras on 26.01.2021.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage into CIImage!")
+            }
+            
+            detect(image: ciimage)
+        }
+
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed!")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to precess image!")
+            }
+            
+            if let firstResul = results.first {
+                self.navigationItem.title = firstResul.identifier.description;
+//                if firstResul.identifier.contains("hotdog") {
+//                    self.navigationItem.title = "Hotdog!"
+//                } else {
+//                    self.navigationItem.title = "Not Hotdog!"
+//                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+      
+    }
+
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+}
+
